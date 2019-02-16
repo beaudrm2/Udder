6732eea844574711bc9ab02c33aab3daa9f31605
Marc Beaudry <beaudry.marc@gmail.com>
2019-02-16 09:32:01 -0500

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Marc on 2019-02-16.
+//  Copyright © 2019 Marc Beaudry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
